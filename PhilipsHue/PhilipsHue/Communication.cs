4db159974770e8f7f0893d55ce1e7936ddd29c21
Mauro de Lyon <maurodelyon@gmail.com>
2015-11-11 12:46:13 +0100

    week1

---
+++ PhilipsHue/PhilipsHue/Communication.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhilipsHue
+{
+    class Communication
+    {
+        public static void SendString(TcpClient client, string message)
+        {
+            StreamWriter stream = new StreamWriter(client.GetStream(), Encoding.Unicode);
+            stream.WriteLine(message);
+            stream.Flush();
+        }
+
+        public static string ReadString(TcpClient client)
+        {
+            StreamReader stream = new StreamReader(client.GetStream(), Encoding.Unicode);
+            return stream.ReadLine();
+        }
+    }
+}
---
+++ PhilipsHue/PhilipsHue/LampSturing.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhilipsHue
+{
+    class LampSturing
+    {
+        public TcpClient client;
+        private List<lamp> lampen;
+       
+        public LampSturing(TcpClient client)
+        {
+            this.client = client;
+            lampen = new List<lamp>();
+            lampen.Add(new lamp(1));
+        }
+
+        public void setLightState(string id, string bri, string hue, string sat)
+        {
+            foreach (lamp lamp in lampen)
+            {
+                int idt = Convert.ToInt32(id);
+                if (lamp.id == idt)
+                {
+                    lamp.bri = Convert.ToInt32(bri);
+                    lamp.hue = Convert.ToInt32(hue);
+                    lamp.sat = Convert.ToInt32(sat);
+                    Console.WriteLine($"lamp {lamp.id} set to {lamp.bri} brightness, {lamp.hue} hue and {lamp.sat} saturation");
+                }
+            }
+
+        }
+
+        public void setBrightness(string id, string bri)
+        {
+            foreach (lamp lamp in lampen)
+            {
+                int idt = Convert.ToInt32(id);
+                if (lamp.id == idt)
+                {
+                    lamp.bri = Convert.ToInt32(bri);
+                    Console.WriteLine($"lamp {lamp.id} set to {lamp.bri} brightness, {lamp.hue} hue and {lamp.sat} saturation");
+                }
+            }
+        }
+
+        public void setHue(string id, string hue)
+        {
+            foreach (lamp lamp in lampen)
+            {
+                int idt = Convert.ToInt32(id);
+                if (lamp.id == idt)
+                {
+                    lamp.hue = Convert.ToInt32(hue);
+                    Console.WriteLine($"lamp {lamp.id} set to {lamp.bri} brightness, {lamp.hue} hue and {lamp.sat} saturation");
+                }
+            }
+        }
+
+        public void setSaturation(string id, string sat)
+        {
+            foreach (lamp lamp in lampen)
+            {
+                int idt = Convert.ToInt32(id);
+                if (lamp.id == idt)
+                {
+                    lamp.sat = Convert.ToInt32(sat);
+                    Console.WriteLine($"lamp {lamp.id} set to {lamp.bri} brightness, {lamp.hue} hue and {lamp.sat} saturation");
+                }
+            }
+        }
+    }
+    class lamp
+    {
+        public int id;
+        public int bri;
+        public int hue;
+        public int sat;
+
+        public lamp(int id)
+        {
+            this.id = id;
+            bri = 0;
+            hue = 0;
+            sat = 0;
+        }
+    }
+}
